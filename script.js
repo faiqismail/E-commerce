9e61bba00562cf7af4cd0a1b214a766588a227d1
faiqismail <faiq69bikejr@gmail.com>
2022-09-02 11:37:53 -1200

    e-commerce

---
+++ script.js
@@ -0,0 +1,145 @@
+const concurrently = require('concurrently');
+const upath = require('upath');
+
+const browserSyncPath = upath.resolve(upath.dirname(__filename), '../node_modules/.bin/browser-sync');
+
+concurrently([
+    { command: 'node --inspect scripts/sb-watch.js', name: 'SB_WATCH', prefixColor: 'bgBlue.bold' },
+    { 
+        command: `${browserSyncPath} dist -w --no-online`,
+        name: 'SB_BROWSER_SYNC', 
+        prefixColor: 'bgBlue.bold',
+    }
+], {
+    prefix: 'name',
+    killOthers: ['failure', 'success'],
+}).then(success, failure);
+
+function success() {
+    console.log('Success');    
+}
+
+function failure() {
+    console.log('Failure');
+}
+const _ = require('lodash');
+const chokidar = require('chokidar');
+const upath = require('upath');
+const renderAssets = require('./render-assets');
+const renderPug = require('./render-pug');
+const renderScripts = require('./render-scripts');
+const renderSCSS = require('./render-scss');
+
+const watcher = chokidar.watch('src', {
+    persistent: true,
+});
+
+let READY = false;
+
+process.title = 'pug-watch';
+process.stdout.write('Loading');
+let allPugFiles = {};
+
+watcher.on('add', filePath => _processFile(upath.normalize(filePath), 'add'));
+watcher.on('change', filePath => _processFile(upath.normalize(filePath), 'change'));
+watcher.on('ready', () => {
+    READY = true;
+    console.log(' READY TO ROLL!');
+});
+
+_handleSCSS();
+
+function _processFile(filePath, watchEvent) {
+    
+    if (!READY) {
+        if (filePath.match(/\.pug$/)) {
+            if (!filePath.match(/includes/) && !filePath.match(/mixins/) && !filePath.match(/\/pug\/layouts\//)) {
+                allPugFiles[filePath] = true;
+            }    
+        }    
+        process.stdout.write('.');
+        return;
+    }
+
+    console.log(`### INFO: File event: ${watchEvent}: ${filePath}`);
+
+    if (filePath.match(/\.pug$/)) {
+        return _handlePug(filePath, watchEvent);
+    }
+
+    if (filePath.match(/\.scss$/)) {
+        if (watchEvent === 'change') {
+            return _handleSCSS(filePath, watchEvent);
+        }
+        return;
+    }
+
+    if (filePath.match(/src\/js\//)) {
+        return renderScripts();
+    }
+
+    if (filePath.match(/src\/assets\//)) {
+        return renderAssets();
+    }
+
+}
+
+function _handlePug(filePath, watchEvent) {
+    if (watchEvent === 'change') {
+        if (filePath.match(/includes/) || filePath.match(/mixins/) || filePath.match(/\/pug\/layouts\//)) {
+            return _renderAllPug();
+        }
+        return renderPug(filePath);
+    }
+    if (!filePath.match(/includes/) && !filePath.match(/mixins/) && !filePath.match(/\/pug\/layouts\//)) {
+        return renderPug(filePath);
+    }
+}
+
+function _renderAllPug() {
+    console.log('### INFO: Rendering All');
+    _.each(allPugFiles, (value, filePath) => {
+        renderPug(filePath);
+    });
+}
+
+function _handleSCSS() {
+    renderSCSS();
+}
+
+
+
+const autoprefixer = require('autoprefixer')
+const fs = require('fs');
+const packageJSON = require('../package.json');
+const upath = require('upath');
+const postcss = require('postcss')
+const sass = require('sass');
+const sh = require('shelljs');
+
+const stylesPath = '../src/scss/styles.scss';
+const destPath = upath.resolve(upath.dirname(__filename), '../dist/css/styles.css');
+
+module.exports = function renderSCSS() {
+    
+    const results = sass.renderSync({
+        data: entryPoint,
+        includePaths: [
+            upath.resolve(upath.dirname(__filename), '../node_modules')
+        ],
+      });
+
+    const destPathDirname = upath.dirname(destPath);
+    if (!sh.test('-e', destPathDirname)) {
+        sh.mkdir('-p', destPathDirname);
+    }
+
+    postcss([ autoprefixer ]).process(results.css, {from: 'styles.css', to: 'styles.css'}).then(result => {
+        result.warnings().forEach(warn => {
+            console.warn(warn.toString())
+        })
+        fs.writeFileSync(destPath, result.css.toString());
+    })
+
+};
+
